6e399f2b19bc6e16ceb770b3ac315f2658e7c18e
Gil Arbeli <gila@dream-security.com>
2023-12-14 15:28:55 +0200

    move some stuff around, use error types, unittests and unittests helpers

---
+++ pkg/common/handler_extra_test.go
@@ -0,0 +1,13 @@
+package common
+
+func ResetHandlers() {
+	hMap = &handlerMap{}
+}
+
+func CountInitHandlers() (count int) {
+	initMap.Range(func(key, value any) bool {
+		count += 1
+		return true
+	})
+	return
+}
